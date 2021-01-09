3d288b304e916d1f937376cd1d2d242d0af0e80a
Alexssandro Rodrigues Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2021-01-09 19:19:07 -0200

    Exemple for Account Bank in Java POO!

---
+++ Documents/NetBeansProjects/MyProject_AccountBank/src/myproject_accountbank/MyProject_AccountBank.java
@@ -0,0 +1,35 @@
+package myproject_accountbank;
+
+/**
+ *
+ * @author Alexssandro
+ */
+public class MyProject_AccountBank {
+
+
+    public static void main(String[] args) {
+        AccountBank p1 = new AccountBank();
+        p1.setNumConta (1111);
+        p1.setDono ("Alexssandro");
+        p1.abrirConta("CC");
+        p1.depositar(100);
+        p1.sacar(150);
+        
+        
+        
+        AccountBank p2 = new AccountBank();
+        p2.setNumConta (2222);
+        p2.setDono ("Valdemir");
+        p2.abrirConta ("CP");
+        p2.depositar(300);
+        p2.sacar(450);
+        
+  
+        p1.estadoAtual();
+        p2.estadoAtual();
+        
+        
+        
+ }
+    
+}
